656b080004504da6b889cb8fc8fc504eb60d0ec7
Piyush Kalyan <79275157+PiyushKalyanpy@users.noreply.github.com>
2021-10-22 11:58:50 +0530

    Delete thisme.js

--- thisme.js
+++
@@ -1,2 +0,0 @@
- $(this).data('song'); // getting song name from _data_ container of clicked element $(this)
-        $('#myAudio').attr('src',songFileName).play(); // getting element myAudio bi its id, then setting it's src attribute and, finally, starting playback 
